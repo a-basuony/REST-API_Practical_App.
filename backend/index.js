b3e56b7a7f2d986b411aa71b92221411ca11fd99
Ahmed Basuony <A.Basuony.social@gmail.com>
2025-09-23 16:42:14 +0300

    adding GraphQL

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,10 @@ const dotenv = require("dotenv");
 const path = require("path");
 
 const multer = require("multer");
-
-const feedRoutes = require("./routes/post.routes");
-const authRoutes = require("./routes/auth.routes");
+const { graphqlHTTP } = require("express-graphql");
 const connectDB = require("./config/db");
-const { init } = require("./socket");
+const graphqlSchema = require("./graphql/schema");
+const graphqlResolvers = require("./graphql/resolvers");
 
 const app = express();
 dotenv.config();
@@ -49,9 +48,13 @@ app.use(
   multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
 );
 
-// Routes
-app.use("/feed", feedRoutes);
-app.use("/auth", authRoutes);
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolvers,
+  })
+);
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -74,18 +77,8 @@ app.use((error, req, res, next) => {
 connectDB()
   .then(() => {
     // Start server
-
-    // 🔥 Init socket.io separately
-    const server = http.createServer(app);
-    init(server);
-
-    server.listen(PORT, () => {
+    app.listen(PORT, () => {
       console.log(`🚀 Server running on port ${PORT} and Database connected`);
-    });
-
-    const io = require("socket.io")(server);
-    io.on("connection", (socket) => {
-      console.log("Client connected");
     });
   })
   .catch((error) => {
